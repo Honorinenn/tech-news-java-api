155bb3c9cafed2f5684d747d6e2f03e02a0f2574
Honorine Ndom <honorinendzah@gmail.com>
2022-03-29 05:51:58 -0700

    added VariablePractice.java

--- VariablePractice.java
+++ VariablePractice.java
@@ -1,10 +1,5 @@
 public class VariablePractice {
-    public static void main(String[] args) {
-
-    }
-}
-public class VariableTypes {
-    public static void main(String[] args) {
+    public static void main(int[] args) {
 
     }
 }
