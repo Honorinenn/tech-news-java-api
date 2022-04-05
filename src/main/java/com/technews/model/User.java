524fd12934e101188cd6c587e0f08d35529dd375
Honorine Ndom <honorinendzah@gmail.com>
2022-04-05 08:08:01 -0700

    created User.java file

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,4 +1,9 @@
 package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Entity
 @JsonIgnoreProperities({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
