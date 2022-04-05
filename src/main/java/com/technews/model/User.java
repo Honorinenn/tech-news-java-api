cccf0949cd28540e965f6fd47e3bb434df011d25
Honorine Ndom <honorinendzah@gmail.com>
2022-04-05 08:16:56 -0700

    changes done on User.java file

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -17,4 +17,8 @@ public class User {
     private List<Vote> votes;
     private List<Comment> comments;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 }
+
