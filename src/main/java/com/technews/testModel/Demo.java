44c792fc7b0fd646f4e455ab9fa089fbde5666bb
Honorine Ndom <honorinendzah@gmail.com>
2022-04-05 07:09:53 -0700

    added Demo.java file

--- src/main/java/com/technews/testModel/Demo.java
+++ src/main/java/com/technews/testModel/Demo.java
@@ -4,4 +4,14 @@ public class Demo {
     private String name;
     private int age;
 
+    public Demo(String name) {
+        this.name = name;
+    }
+
+    public Demo(int age) {
+        this.age = age;
+    }
+
+    public Demo() {
+    }
 }
