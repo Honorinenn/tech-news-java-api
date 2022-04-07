03a98027fe1d59429e9c5a2a28768d603387d13d
Honorine Ndom <honorinendzah@gmail.com>
2022-04-06 18:38:19 -0700

    changes done on User.java file

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -18,7 +18,8 @@ public class User {
     // Need to use FetchType.LAZY to resolve multiple bags exception
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Vote> votes;
-
+    // Need to use FetchType.LAZY to resolve multiple bags exception
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Comment> comments;
 
     @Id
